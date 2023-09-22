90ae487b3a3655586320b907cbe381ca0d0f49fb
KarthiR98 <117893319+KarthiR98@users.noreply.github.com>
2023-09-22 14:42:29 +0530

    Update file.php

--- file.php
+++ file.php
@@ -7,6 +7,22 @@
 <?php
 echo "Hello Worldd!";
 ?> 
+<?php
+$brokers = "my-cluster-kafka-bootstrap.karthir.svc:9092"; // Kafka bootstrap server and port
+$topic = "my-topic"; // Kafka topic
 
+$config = new RdKafka\Conf();
+$config->set('bootstrap.servers', $brokers);
+
+$producer = new RdKafka\Producer($config);
+$producer->addBrokers($brokers);
+
+$topic = $producer->newTopic($topic);
+
+$message = "Hello, Kafka!";
+$topic->produce(RD_KAFKA_PARTITION_UA, 0, $message);
+
+echo "Message sent to Kafka: $message\n";
+?>
 </body>
 </html>
